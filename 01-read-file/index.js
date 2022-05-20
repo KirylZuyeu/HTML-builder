50eeda99f2e6cc77e9fa16a1580c67db31381815
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-05-20 13:30:17 +0300

    feat: solution for the first part of task (Fri, May 20, 2022 01:30:17 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathForFile = path.join(__dirname, '/text.txt');
+
+function read(filePath) {
+    const readableStream = fs.createReadStream(filePath, 'utf8');
+    readableStream.on('error', function (error) {
+        console.log(`error: ${error.message}`);
+    });
+    readableStream.on('data', (chunk) => {
+        console.log(chunk);
+    });
+}
+
+read(pathForFile);
