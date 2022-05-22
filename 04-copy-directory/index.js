fb0de44aa1a9ab6ec3cb76d7650e4131f4a446d4
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-05-22 16:44:01 +0300

    feat: add solution for foutrh part of task (Sun, May 22, 2022 04:44:01 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, copyFile, mkdir, stat } = require('fs/promises');
 
+const srcFolder = path.join(__dirname, '\\files');
+const dirFolder = path.join(__dirname, '\\files-copy');
+
+fs.rm(dirFolder, { recursive: true }, () => {
+  copyDir(srcFolder, dirFolder).then();
+});
+
+async function copyDir(src, dest) {
+  var stats = await stat(src);
+  var isDirectory = stats.isDirectory();
+  if (isDirectory) {
+    await mkdir(dest, { recursive: true });
+    (await readdir(src)).forEach(function(childItemName) {
+      copyDir(path.join(src, childItemName),
+        path.join(dest, childItemName));
+    });
+  } else {
+    await copyFile(src, dest);
+  }
+}
