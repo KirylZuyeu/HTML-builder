6ce36ad8cdfd8e69c2e94fa84a24e5be4102497d
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-05-24 16:02:09 +0300

    fix: recreation file txt for second part of task (Tue, May 24, 2022 04:02:09 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,4 @@
-const { createReadStream, createWriteStream } = require('fs');
+const { createWriteStream } = require('fs');
 const { join } = require('path');
 const { createInterface } = require('readline');
 
@@ -9,22 +9,8 @@ const pathForFile = join(__dirname, 'text.txt');
 console.log('Справка: Для прекращения нажмите комбинацию клавиш Ctrl+C или введите строку exit.');
 console.log('Справка: Для сохранения текста в файл, нажмите Enter.');
 
-function read(filePath) {
-  const readableStream = createReadStream(filePath, 'utf8');
-  readableStream.on('error', function (error) {
-    console.log(`error: ${error.message}`);
-  });
-  readableStream.on('data', (chunk) => {
-    console.log(chunk);
-  });
-}
-
 function write(filePath) {
-  const writableStream = createWriteStream(filePath, { flags: 'a',
-    encoding: null,
-    // eslint-disable-next-line no-octal
-    mode: 0666 
-  });
+  const writableStream = createWriteStream(filePath, 'utf-8');
 
   writableStream.on('error',  (error) => {
     console.log(`\nAn error occured while writing to the file. Error: ${error.message}`);
@@ -42,7 +28,6 @@ function write(filePath) {
     let sentence;
     switch (line.trim()) {
     case 'exit':
-      read(filePath);
       rl.close();
       break;
     default:
